f2ebf776370bc47480f5d665bf6708b13aa3f384
Guy Barnard <guy@domabo.com>
2017-02-08 17:07:45 -0600

    working for cordova/widget xsd

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var xml2js = require('xml2js');
+var xml2js_processors = require( 'xml2js/lib/processors');
 
 var fs = require('fs');
+
+var NAMEVALUES = {
+    'preference': 'value',
+    'param': 'value',
+    'engine': 'spec',
+    'plugin': 'spec'
+};
+var SINGLETONS = { 'allow-intent': '$href' };
+var ATTRIBUTES = require('./widget.json');
+var NAMEOBJECTS = ['platform'];
 
 var x2js = new xml2js.Parser({
     attrkey: '$',
@@ -15,13 +26,34 @@ var x2js = new xml2js.Parser({
     async: false
 });
 
+var xmlStr = fs.readFileSync('./widget.xsd', 'utf-8')
+x2js.parseString(xmlStr, function (e, jsonObj) {
+    var XSD = {};
+    jsonObj = _xsd(XSD, jsonObj);
+    fs.writeFileSync('widget-source.json', JSON.stringify(jsonObj, null, 4), 'utf-8');
+    fs.writeFileSync('widget.json', JSON.stringify(XSD, null, 4), 'utf-8');
+})
+
+var x2js = new xml2js.Parser({
+    attrkey: '$',
+    charkey: 'value',
+    trim: true,
+    normalize: true,
+    normalizeTags: false,
+    explicitRoot: false,
+    explicitArray: false,
+    attrNameProcessors: [function (name) { return "$" + name }],
+    attrValueProcessors: [xml2js_processors.parseNumbers, xml2js_processors.parseBooleans],
+    mergeAttrs: true,
+    async: false
+});
+
 var xmlStr = fs.readFileSync('./config.xml', 'utf-8')
 x2js.parseString(xmlStr, function (e, jsonObj) {
-    jsonObj = _simplify(jsonObj);
+    jsonObj = _simplify("widget", jsonObj);
     var contents = JSON.stringify(jsonObj, null, 4);
-    console.log(contents);
     fs.writeFileSync('config.json', contents, 'utf-8');
-    setTimeout(process.exit, 1)
+
 })
 
 var x2js2 = new xml2js.Builder({
@@ -36,40 +68,16 @@ var x2js2 = new xml2js.Builder({
 });
 
 var jsonObj = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
-jsonObj = _unsimplify(jsonObj);
-jsonObj = render(jsonObj);
+jsonObj = _unsimplify("widget", jsonObj);
+fs.writeFileSync('config2.json', JSON.stringify(jsonObj, null, 4), 'utf-8');
+jsonObj = _promoteAttributes(jsonObj);
 var contents = x2js2.buildObject(jsonObj);
-console.log(contents);
 fs.writeFileSync('config2.xml', contents, 'utf-8');
+process.exit()
 
-function render(obj) {
+function _xsd(XSD, obj) {
     var attr, child, entry, index, key, value;
-    if (typeof obj !== 'object') {
-        return obj;
-    } else {
-        for (key in obj) {
-            if (!Object.hasOwnProperty.call(obj, key)) continue;
-            child = obj[key];
-            if (key.startsWith('$')) {
-                obj['$'] = obj['$'] || {};
-                obj['$'][key.substr(1)] = render(child);
-                delete obj[key];
-            } else
-                obj[key] = render(child)
-        }
-        return obj;
-    };
-}
 
-
-
-function _simplify(obj) {
-    var ATTRIBUTES = ['href', 'email', 'src', 'id', 'version', 'origin', 'xmlns']
-    var SINGLETONS = ['allow-intent']
-    var NAMEVALUES = ['preference']
-
-    var SINGLETONS_DECODE = {'allow-intent': '$href'}
-    var attr, child, entry, index, key, value;
     if (typeof obj !== 'object') {
         return obj;
     } else {
@@ -77,197 +85,231 @@ function _simplify(obj) {
             if (!Object.hasOwnProperty.call(obj, key)) continue;
 
             child = obj[key];
-            if (key.startsWith('$') && ATTRIBUTES.includes(key.substr(1))) {
-                obj[key.substr(1)] = _simplify(child);
-                delete obj[key];
-            } else if (SINGLETONS.includes(key)) {
-                var decode = SINGLETONS_DECODE[key];
-                var singletonresult = [];
-                 for (index in child) {
-                        if (!Object.hasOwnProperty.call(child, index)) continue;
-                        entry = child[index];
-                        singletonresult.push(entry[decode])
-                      
-                    }
-                delete obj[key];
-                obj[key] =singletonresult;
-            } else if (NAMEVALUES.includes(key)) {
-                var namevalueresult = {};
-                 for (index in child) {
-                        if (!Object.hasOwnProperty.call(child, index)) continue;
-                        entry = child[index];
-                        namevalueresult[entry['$name']] = entry['$value'];      
-                    }
-                delete obj[key];
-                obj[key] =namevalueresult;
-            } else {
+            if (key == "xs:element") {
                 if (Array.isArray(child)) {
                     for (index in child) {
                         if (!Object.hasOwnProperty.call(child, index)) continue;
                         entry = child[index];
-                        if (typeof entry === 'string') {
-                            // no change
-                        } else {
-                            child[index] = _simplify(entry);
-                        }
-                    }
-                } else if (typeof child === "object") {
-                    delete obj[key];
-                    obj[key] = _simplify(child)
-                } else {
-                    entry = obj[key];
-                    delete obj[key];
-                    obj[key] = entry;
-                }
-            }
-        }
-
-        return obj;
-    };
-}
-
-function _unsimplify(obj) {
-    var ATTRIBUTES = ['href', 'email', 'src', 'id', 'version', 'origin', 'xmlns']
-    var attr, child, entry, index, key, value;
-    if (typeof obj !== 'object') {
-        return obj;
-    } else {
-        for (key in obj) {
-            if (!Object.hasOwnProperty.call(obj, key)) continue;
-
-            child = obj[key];
-            if (ATTRIBUTES.includes(key)) {
-                obj['$' + key] = _unsimplify(child);
-                delete obj[key];
-            } else {
-                if (Array.isArray(child)) {
-                    for (index in child) {
-                        if (!Object.hasOwnProperty.call(child, index)) continue;
-                        entry = child[index];
-                        if (typeof entry === 'string') {
-                            // no change
-                        } else {
-                            child[index] = _unsimplify(entry);
-                        }
-                    }
-                } else if (typeof child === "object") {
-                    obj[key] = _unsimplify(child)
-                } else {
-                    // no change for string etc
-                }
-            }
-        }
-
-        return obj;
-    };
-}
-
-
-function _render(obj) {
-    var attr, child, entry, index, key, value;
-    if (typeof obj !== 'object') {
-        return obj;
-    } else {
-        for (key in obj) {
-            if (!Object.hasOwnProperty.call(obj, key)) continue;
-
-            child = obj[key];
-
-            if (key.startsWith('$')) {
-                obj['$'] = obj['$'] || {};
-                obj['$'][key.substr(1)] = _render(child);
-                delete obj[key];
-            } else {
-                if (Array.isArray(child)) {
-                    for (index in child) {
-                        if (!Object.hasOwnProperty.call(child, index)) continue;
-                        entry = child[index];
-                        if (typeof entry === 'string') {
-                            // no change
-                        } else {
-                            child[index] = _render(entry);
-                        }
-                    }
-                } else if (typeof child === "object") {
-                    obj[key] = _render(child)
-                } else {
-                    // no change for string etc
-                }
-            }
-        }
-
-        return obj;
-    };
-}
-
-
-function Simplify(rootObj) {
-    var attrkey, charkey, render, rootElement, rootName;
-    attrkey = this.options.attrkey;
-    charkey = this.options.charkey;
-    if ((Object.keys(rootObj).length === 1)) {
-        rootName = Object.keys(rootObj)[0];
-        rootObj = rootObj[rootName];
-    } else {
-        rootName = 'widget';
-    }
-    render = (function (_this) {
-        return function (element, obj) {
-            var attr, child, entry, index, key, value;
-            if (typeof obj !== 'object') {
-                element.txt(obj);
-            } else {
-                for (key in obj) {
-                    if (!hasProp.call(obj, key)) continue;
-                    child = obj[key];
-                    if (key === attrkey) {
-                        if (typeof child === "object") {
-                            for (attr in child) {
-                                value = child[attr];
-                                element = element.att(attr, value);
-                            }
-                        }
-                    } else if (key === charkey) {
-                        if (_this.options.cdata && requiresCDATA(child)) {
-                            element = element.raw(wrapCDATA(child));
-                        } else {
-                            element = element.txt(child);
-                        }
-                    } else if (Array.isArray(child)) {
-                        for (index in child) {
-                            if (!hasProp.call(child, index)) continue;
-                            entry = child[index];
-                            if (typeof entry === 'string') {
-                                if (_this.options.cdata && requiresCDATA(entry)) {
-                                    element = element.ele(key).raw(wrapCDATA(entry)).up();
-                                } else {
-                                    element = element.ele(key, entry).up();
+                        if (entry["xs:complexType"] && entry["xs:complexType"]["xs:attribute"]) {
+                            objectMap(entry["xs:complexType"]["xs:attribute"], function (attrentry) {
+                                if (attrentry["$name"]) {
+                                    XSD[entry["$name"]] = XSD[entry["$name"]] || [];
+                                    XSD[entry["$name"]].push(attrentry["$name"])
                                 }
-                            } else {
-                                element = render(element.ele(key), entry).up();
-                            }
+                            });
                         }
-                    } else if (typeof child === "object") {
-                        element = render(element.ele(key), child).up();
-                    } else {
-                        if (typeof child === 'string' && _this.options.cdata && requiresCDATA(child)) {
-                            element = element.ele(key).raw(wrapCDATA(child)).up();
-                        } else {
-                            if (child == null) {
-                                child = '';
+                    }
+                } else {
+                    entry = child;
+                    if (entry["xs:complexType"] && entry["xs:complexType"]["xs:attribute"]) {
+                        objectMap(entry["xs:complexType"]["xs:attribute"], function (attrentry) {
+                            if (attrentry["$name"]) {
+                                XSD[entry["$name"]] = XSD[entry["$name"]] || [];
+                                XSD[entry["$name"]].push(attrentry["$name"])
                             }
-                            element = element.ele(key, child.toString()).up();
-                        }
+                        })
                     }
                 }
             }
-            return element;
-        };
-    })(this);
-    rootElement = builder.create(rootName, this.options.xmldec, this.options.doctype, {
-        headless: this.options.headless,
-        allowSurrogateChars: this.options.allowSurrogateChars
-    });
-    return render(rootElement, rootObj).end(this.options.renderOpts);
-};
+             if (Array.isArray(child)) {
+                for (index in child) {
+                    if (!Object.hasOwnProperty.call(child, index)) continue;
+                    entry = child[index];
+                    if (typeof entry === 'string') {
+                        // no change
+                    } else {
+                        child[index] = _xsd(XSD, entry);
+                    }
+                }
+            } else if (typeof child === "object") {
+                delete obj[key];
+                obj[key] = _xsd(XSD, child)
+            } else {
+                entry = obj[key];
+                delete obj[key];
+                obj[key] = entry;
+            }
 
+        }
+        return obj;
+    };
+}
+
+function _simplify(parent, obj) {
+    var attr, child, entry, index, key, value;
+    if (typeof obj !== 'object') {
+        return obj;
+    } else {
+        for (key in obj) {
+            if (!Object.hasOwnProperty.call(obj, key)) continue;
+            child = obj[key];
+            if (key.startsWith('$') && ATTRIBUTES[parent] && ATTRIBUTES[parent].includes(key.substr(1))) {
+                obj[key.substr(1)] = _simplify(key, child);
+                delete obj[key];
+            } else if (SINGLETONS[key]) {
+                var decode = SINGLETONS[key];
+                var singletonresult = [];
+                objectMap(child, function (entry) {
+                    singletonresult.push(entry[decode])
+                })
+                delete obj[key];
+                obj[key] = singletonresult;
+            } else if (NAMEOBJECTS.includes(key)) {
+                var nameobjectresult = {};
+                objectMap(child, function (entry) {
+                    index = entry['$name'];
+                    delete entry['$name'];
+                    nameobjectresult[index] = _simplify(key, entry);
+                })
+                delete obj[key];
+                obj[key] = nameobjectresult;
+            } else if (NAMEVALUES[key]) {
+                var valueitem = '$' + NAMEVALUES[key];
+                var namevalueresult = {};
+                objectMap(child, function (entry) {
+                    namevalueresult[entry['$name']] = entry[valueitem];
+                });
+                delete obj[key];
+                obj[key] = namevalueresult;
+            } else {
+                if (Array.isArray(child)) {
+                    for (index in child) {
+                        if (!Object.hasOwnProperty.call(child, index)) continue;
+                        entry = child[index];
+                        if (typeof entry === 'string') {
+                            // no change
+                        } else {
+                            child[index] = _simplify(key, entry);
+                        }
+                        delete obj[key];
+                        obj[key] = child;
+                    }
+                } else if (typeof child === "object") {
+                    delete obj[key];
+                    obj[key] = _simplify(key, child)
+                } else {
+                    delete obj[key];
+                    obj[key] = child;
+                }
+            }
+        }
+
+        return obj;
+    };
+}
+
+function _unsimplify(parent, obj) {
+    var attr, child, entry, index, key, value;
+    if (typeof obj !== 'object') {
+        return obj;
+    } else {
+        for (key in obj) {
+            if (!Object.hasOwnProperty.call(obj, key)) continue;
+
+            child = obj[key];
+            if (ATTRIBUTES[parent] && ATTRIBUTES[parent].includes(key)) {
+                obj['$' + key] = _unsimplify(key, child);
+                delete obj[key];
+            } else if (SINGLETONS[key]) {
+                var decode = SINGLETONS[key];
+                var singletonresult = [];
+                objectMap(child, function (entry) {
+                   singletonresult.push({ [decode]: entry })
+                })
+                delete obj[key];
+                obj[key] = singletonresult;
+            } else if (NAMEOBJECTS.includes(key)) {
+                var nameobjectresult = [];
+                for (index in child) {
+                    if (!Object.hasOwnProperty.call(child, index)) continue;
+                    entry = _unsimplify(key, child[index]);
+                    entry['$name'] = index;
+                    nameobjectresult.push(entry);
+                }
+                delete obj[key];
+                obj[key] = nameobjectresult;
+            } else if (NAMEVALUES[key]) {
+                var decode = '$' + NAMEVALUES[key];
+                var namevalueresult = [];
+                for (index in child) {
+                    if (!Object.hasOwnProperty.call(child, index)) continue;
+                    entry = child[index];
+                    namevalueresult.push({ "$name": index, [decode]: entry })
+                }
+                delete obj[key];
+                obj[key] = namevalueresult;
+            } else if (Array.isArray(child)) {
+                for (index in child) {
+                    if (!Object.hasOwnProperty.call(child, index)) continue;
+                    entry = child[index];
+                    if (typeof entry === 'string') {
+                        // no change
+                    } else {
+                        child[index] = _unsimplify(key, entry);
+                    }
+                }
+                delete obj[key];
+                obj[key] = child;
+            } else if (typeof child === "object") {
+                delete obj[key];
+                obj[key] = _unsimplify(key, child)
+            } else {
+                delete obj[key];
+                obj[key] = child;
+                // no change for string etc
+            }
+        }
+    }
+
+    return obj;
+}
+
+function _promoteAttributes(obj) {
+    var attr, child, entry, index, key, value;
+    if (typeof obj !== 'object') {
+        return obj;
+    } else {
+        for (key in obj) {
+            if (!Object.hasOwnProperty.call(obj, key)) continue;
+
+            child = obj[key];
+
+            if (key.startsWith('$')) {
+                obj['$'] = obj['$'] || {};
+                obj['$'][key.substr(1)] = _promoteAttributes(child);
+                delete obj[key];
+            } else {
+                if (Array.isArray(child)) {
+                    for (index in child) {
+                        if (!Object.hasOwnProperty.call(child, index)) continue;
+                        entry = child[index];
+                        if (typeof entry === 'string') {
+                            // no change
+                        } else {
+                            child[index] = _promoteAttributes(entry);
+                        }
+                    }
+                } else if (typeof child === "object") {
+                    obj[key] = _promoteAttributes(child)
+                } else {
+                    // no change for string etc
+                }
+            }
+        }
+
+        return obj;
+    };
+}
+
+function objectMap(obj, func, valuefunc) {
+    if (Array.isArray(obj)) {
+        for (index in obj) {
+            if (!Object.hasOwnProperty.call(obj, index)) continue;
+            func(obj[index]);
+        }
+    } else if (typeof obj === 'object') {
+        func(obj);
+    } else if (valuefunc) {
+        valuefunc(obj);
+    }
+}
